7df8a6ba05198a88b744ea9d1e301747b252bc39
Topher Dunlap <thelonioustopher@yahoo.com>
2021-07-19 15:55:25 -0500

    reducing console errors

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,7 @@ import '../css/home.css';
 
 export default function Home() {
 
-    return(
-        <body>
+    return (
         <div className="container">
             <header>
                 <div className="wrapper">
@@ -18,27 +17,31 @@ export default function Home() {
                     <h2>Home</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ornare diam neque. Cras semper
                         nisi sed
-                        ante placerat pulvinar. Nunc lacinia, ante nec tristique hendrerit, mauris est accumsan ante,
+                        ante placerat pulvinar. Nunc lacinia, ante nec tristique hendrerit, mauris est accumsan
+                        ante,
                         non
-                        bibendum arcu libero et neque. Cras consequat neque tristique quam pretium, eget rutrum ligula
+                        bibendum arcu libero et neque. Cras consequat neque tristique quam pretium, eget rutrum
+                        ligula
                         vestibulum. Integer sagittis odio eu mauris pulvinar sagittis. Etiam eros tortor, finibus ac
                         tellus non,
                         congue mollis odio. Quisque lacinia diam lacus, imperdiet vestibulum justo pretium id. Donec
                         imperdiet
                         luctus orci quis scelerisque.</p>
-                    <p>Sed euismod pretium purus. Pellentesque habitant morbi tristique senectus et netus et malesuada
+                    <p>Sed euismod pretium purus. Pellentesque habitant morbi tristique senectus et netus et
+                        malesuada
                         fames ac
-                        turpis egestas. Nunc id odio tortor. Ut vulputate id diam vel vestibulum. Proin sed porta purus.
+                        turpis egestas. Nunc id odio tortor. Ut vulputate id diam vel vestibulum. Proin sed porta
+                        purus.
                         Etiam
                         rutrum ultrices elit, id fermentum purus posuere tempor.</p>
                 </article>
             </main>
             <aside>
                 <h3>Featured Agent</h3>
-                <p>Aliquam maximus augue in lacus tincidunt semper. Mauris a felis lectus. Proin lacinia massa id mauris
+                <p>Aliquam maximus augue in lacus tincidunt semper. Mauris a felis lectus. Proin lacinia massa id
+                    mauris
                     dignissim vulputate. Nullam in nisi sapien. Quisque eu turpis urna.</p>
             </aside>
         </div>
-        </body>
     )
 }
